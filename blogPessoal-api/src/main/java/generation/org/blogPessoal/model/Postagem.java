ec40beded2ae7d841b7e58a676aec20d6e6134e7
leticia-sena <everton4732@gmail.com>
2020-11-08 21:41:34 -0300

    CRUD blogPessoal

--- blogPessoal-api/src/main/java/generation/org/blogPessoal/model/Postagem.java
+++ blogPessoal-api/src/main/java/generation/org/blogPessoal/model/Postagem.java
@@ -1,4 +1,4 @@
-package org.generation.blogPessoal.model;
+package generation.org.blogPessoal.model;
 
 import java.util.Date;
 
@@ -6,59 +6,76 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
 
-
-
 @Entity
-@Table (name = "postagem")
+@Table(name = "postagem")
 public class Postagem {
 	
-	@Id
+	@Id	
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
 	@NotNull
-	
+	@Size(min = 5, max = 100)
 	private String titulo;
 	
-	@Autowired
 	@NotNull
-	
-	private String texto;
-	
+	@Size(min = 10, max = 500)
+	private String texto;	
+
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date date = new java.sql.Date(System.currentTimeMillis());
+    private Date data = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+
 	public long getId() {
 		return id;
 	}
+	
 	public void setId(long id) {
 		this.id = id;
 	}
+	
 	public String getTitulo() {
 		return titulo;
 	}
+	
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+	
 	public String getTexto() {
 		return texto;
 	}
+	
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
-	public Date getDate() {
-		return date;
-	}
-	public void setDate(Date date) {
-		this.date = date;
+	
+	public Date getData() {
+		return data;
 	}
 	
+	public void setData(Date data) {
+		this.data = data;
+	}
 	
+	public Tema getTema() {
+		return tema;
+	}
+	
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+		
 }
